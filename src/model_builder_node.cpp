25100b58f7864bde7065b66d91c23deda3ac9f9a
Arkadiusz Mula <arekmula97@gmail.com>
2021-03-17 19:32:40 +0100

    Converted point cloud to image and published it for further processing

--- src/model_builder_node.cpp
+++ src/model_builder_node.cpp
@@ -8,13 +8,24 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-ros::Publisher g_test_point_pub;
+ros::Publisher g_processed_point_cloud_pub;
+ros::Publisher g_image_from_pcl_pub;
 
 void pointCallback(const sensor_msgs::PointCloud2ConstPtr& input_point_cloud)
 {
+    // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+    pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud;
+    pcl::fromROSMsg(*input_point_cloud, pcl_cloud);
+
+    // Get RGB image from PointCloud and publish it so other nodes can generate predictions
+    sensor_msgs::Image rgb_image;
+    pcl::toROSMsg(pcl_cloud, rgb_image);
+    g_image_from_pcl_pub.publish(rgb_image);
+
+    // Publish current processed point lcoud
     sensor_msgs::PointCloud2 output_point_cloud;
     output_point_cloud = *input_point_cloud;
-    g_test_point_pub.publish(output_point_cloud);
+    g_processed_point_cloud_pub.publish(output_point_cloud);
 }
 
 int main(int argc, char **argv)
@@ -24,8 +35,9 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     // Create a ROS publisher for the output point cloud
-    g_test_point_pub = n.advertise<sensor_msgs::PointCloud2>("test_point_cloud", 1000);
-
+    g_processed_point_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("processed_point_cloud", 1000);
+    // Create a ROS publisher for the image generated from point cloud
+    g_image_from_pcl_pub = n.advertise<sensor_msgs::Image>("image_to_process", 1000);
     // ROS subscriber for the input point cloud
     ros::Subscriber point_cloud_sub = n.subscribe("/hz_points", 1, pointCallback);
 
@@ -36,7 +48,6 @@ int main(int argc, char **argv)
         ros::spinOnce();
         loop_rate.sleep();
     }
-
 
     return 0;
 }
